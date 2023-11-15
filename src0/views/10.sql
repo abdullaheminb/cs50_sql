3b34666908b19654e1d0362e5fa3a1d471652980
Abdullah Bayraklilar <abdullahbayraklilar@gmail.com>
2023-11-15 15:41:09 +0300

    All the files I took note during first class.

---
+++ src0/views/10.sql
@@ -0,0 +1,4 @@
+In 10.sql, write a SQL query to answer a question of your choice about the prints. The query should:
+Make use of AS to rename a column
+
+SELECT english_title FROM views AS 'title';
