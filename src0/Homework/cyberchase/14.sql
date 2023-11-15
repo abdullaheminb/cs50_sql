7e0fb9ecdc18613d3af61616129d1ef38865d97e
Abdullah Bayraklilar <abdullahbayraklilar@gmail.com>
2023-11-15 15:42:50 +0300

    Folder Update

---
+++ src0/Homework/cyberchase/14.sql
@@ -0,0 +1,3 @@
+Write a SQL query to find the titles of episodes that have aired during the holiday season, usually in December in the United States.
+
+SELECT title FROM episodes WHERE air_date LIKE '_____12%';
